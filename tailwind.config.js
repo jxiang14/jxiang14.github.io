58722538309819626943951028f9faa8062e491f
Jeffrey <jjx5@cornell.edu>
2023-12-07 03:59:48 -0500

    Changed config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {fontFamily: {
+      hedvig: ["'Hedvig Letters Sans', sans-serif"],
+    },},
   },
   plugins: [],
 }
